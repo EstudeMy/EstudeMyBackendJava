9bbacbb487670a8985402f40c0dbdb63256bb629
João Victor Quaresma de Arruda <joaovictorquaresma2001@gmail.com>
2025-09-07 21:04:15 -0300

    CORS
    Conexão com o FrontEnd.

--- backend/src/main/java/com/estudemy/backend/BackendApplication.java
+++ backend/src/main/java/com/estudemy/backend/BackendApplication.java
@@ -1,9 +1,14 @@
 package com.estudemy.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@AutoConfiguration
+
 public class BackendApplication {
 
     public static void main(String[] args) {
